651cae811e9e64c52d1428f6212c17feaaafe194
Marcel Egger <marcele1987@outlook.com>
2018-11-14 22:12:19 +0000

    Add some comments

--- FunctionApp1/FunctionStartup.cs
+++ FunctionApp1/FunctionStartup.cs
@@ -14,6 +14,7 @@ namespace FunctionApp1
     {
         public void Configure(IWebJobsBuilder builder)
         {
+            //Get the current config and merge it into a new ConfigurationBuilder to keep the old settings
             var configurationBuilder = new ConfigurationBuilder();
             var descriptor = builder.Services.FirstOrDefault(d => d.ServiceType == typeof(IConfiguration));
             if (descriptor?.ImplementationInstance is IConfigurationRoot configuration)
@@ -21,17 +22,23 @@ namespace FunctionApp1
                 configurationBuilder.AddConfiguration(configuration);
             }
 
+            //build the config in order to access the appsettings for getting the key vault connection settings
             var config = configurationBuilder.Build();
 
             var vaultUrl = config["VaultUrl"];
             var vaultClientId = config["VaultClientId"];
             var vaultClientSecret = config["VaultClientSecret"];
 
+            //add the key vault to the configuration builder
             configurationBuilder.AddAzureKeyVault(vaultUrl, vaultClientId, vaultClientSecret);
 
+            //build the config again so it has the key vault provider
             config = configurationBuilder.Build();
 
+            //replace the existing config with the new one
             builder.Services.Replace(ServiceDescriptor.Singleton(typeof(IConfiguration), config));
+
+            //add the ConfigProvider if you want to use IConfiguration in your function, the ConfigProvider is just an implementation of IExtensionConfigProvider to give you access to the current IConfiguration
             builder.AddExtension<ConfigProvider>();
         }
     }
